9e35e82bd06e81b21fd5fc3b5e00ae90b3d9e97d
R1a2h3u4l5k <rahulkhairwar01@gmail.com>
2025-06-23 23:54:39 +0530

    first commit

---
+++ NameSpace.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+namespace Cybrom
+{
+    int a=90;
+    int b=20;
+
+     void multiply(){
+
+        cout<<"multiply="<<a*b;
+
+    }
+
+    class bhopal
+    {
+
+        public :int k1,k2;
+
+    }obj;
+
+}
+
+using namespace Cybrom;
+
+int  main (){
+
+    cout<<a+b;cout<<"\n";
+    multiply();
+    obj.k1=300;
+    cout<<"\nvalue of k1="<<obj.k1;
+    cout<<"\n";
+    cout<<b;
+    
+    return 0;
+    
+}
+
