7fbf7d63a342db8564d7864b1f1107c4f3d22b24
R1a2h3u4l5k <rahulkhairwar01@gmail.com>
2025-07-16 23:49:03 +0530

    first commit

---
+++ set.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+#include<set>
+
+int main (){
+
+ set <int>s1;
+set <string>k;
+k.insert({"hello","your","fine","vijay"});
+
+ s1.insert(12);
+ s1.insert(34);
+ s1.insert(45);
+ s1.insert(32);
+
+ for (auto p:s1){
+
+    cout<<p<<"\n";
+ }
+
+ for(auto p:k){
+
+    cout<<p<<"\n";
+ }
+
+}
